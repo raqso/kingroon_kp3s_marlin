b467e33076336bd28f8045416403f4a39fcc7a5a
Adam Rak <raqsasg@gmail.com>
2024-01-24 10:06:09 +0100

    Reduce PWM noise
    http://hitoriblog.com/kingroon_kp3s/docs/customizing_marlin/

--- configs/Configuration.h
+++ configs/Configuration.h
@@ -2058,7 +2058,7 @@
 #define FILAMENT_RUNOUT_SENSOR
 #if ENABLED(FILAMENT_RUNOUT_SENSOR)
 #define FIL_RUNOUT_ENABLED_DEFAULT true // Enable the sensor on startup. Override with M412 followed by M500.
-#define NUM_RUNOUT_SENSORS 1             // Number of sensors, up to one per extruder. Define a FIL_RUNOUT#_PIN for each.
+#define NUM_RUNOUT_SENSORS 1            // Number of sensors, up to one per extruder. Define a FIL_RUNOUT#_PIN for each.
 
 #define FIL_RUNOUT_STATE HIGH // Pin state indicating that filament is NOT present.
 #define FIL_RUNOUT_PULLUP    // Use internal pullup for filament runout pins.
@@ -3680,20 +3680,20 @@
 // Use software PWM to drive the fan, as for the heaters. This uses a very low frequency
 // which is not as annoying as with the hardware PWM. On the other hand, if this frequency
 // is too low, you should also increment SOFT_PWM_SCALE.
-// #define FAN_SOFT_PWM
+#define FAN_SOFT_PWM
 
 // Incrementing this by 1 will double the software PWM frequency,
 // affecting heaters, and the fan if FAN_SOFT_PWM is enabled.
 // However, control resolution will be halved for each increment;
 // at zero value, there are 128 effective control positions.
 // :[0,1,2,3,4,5,6,7]
-#define SOFT_PWM_SCALE 0
+#define SOFT_PWM_SCALE 3
 
 // If SOFT_PWM_SCALE is set to a value higher than 0, dithering can
 // be used to mitigate the associated resolution loss. If enabled,
 // some of the PWM cycles are stretched so on average the desired
 // duty cycle is attained.
-// #define SOFT_PWM_DITHER
+#define SOFT_PWM_DITHER
 
 // @section extras
 
